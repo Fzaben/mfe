d12e1a7adc4a37f8243fa21d3d64e793c3461514
Fzaben <Fzaben>
2022-06-14 00:07:22 +0300

    Updated remoteEntry

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -8,14 +8,14 @@ const domain = process.env.PRODUCTION_DOMAIN;
 const prodConfig = {
   mode: 'production',
   output: {
-    filename: '[name].[chunkhash].js',
-    publicPath: `/container/latest/`
+    filename: '[name].[contenthash].js',
+    publicPath: '/container/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
