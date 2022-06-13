e0094f913e15d95cee908ac7a2ef25e3c8d9f7e9
Fzaben <Fzaben>
2022-06-13 23:58:42 +0300

    Added marketing deployment

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -9,6 +9,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[chunkhash].js",
+    publicPath: '/marketing/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
